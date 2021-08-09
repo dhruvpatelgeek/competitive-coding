a3153c95af8ff8e12d8a88bb5fbce9b0fce34b4c
dhruvpatelgeek <dhruvpatel1999@outlook.in>
2021-08-08 17:25:42 -0700

    added schedule

--- blind75/arrays/MaximumSubarray/MaximumSubarray.cpp
+++ blind75/arrays/MaximumSubarray/MaximumSubarray.cpp
@@ -5,40 +5,59 @@ using namespace std;
 class Solution {
 public:
     int maxSubArray(vector<int>& v) {
-	const int n=(int)v.size();
-	int ans=0; for(int i=0;i<n;i++){
-		ans+=v[i];	
+	const int sz=(int)v.size();
+	int ans=-111;
+	for(int ti=0;i<sz;cout<<v[i++]<<"\t");
+	int **dp= new int*[sz];
+	for(int i=0;i<sz;i++){
+		dp[i]=new int[sz];
+		memset(dp[i],0xffffffff,sizeof(int)*sz);
 	}
-	int leftLim=0; 
-	int leftsum=v[0]; 
-	for(int i=1;i<n;i++){
-		leftsum+=v[i];
-		if((ans-leftsum)>ans){
-			ans-=leftsum;
-			leftsum=0;
-			leftLim=i;
+
+	for(int i=0;i<sz;i++){
+		cout<<endl;
+		for(int j=0;j<sz;j++){
+			if(j<i){
+				cout<<"\t";
+			} else {
+				cout<<i<<","<<j<<"\t";	
+				//cout<<dp[i][j]<<"\t";
+			}
 		}
 	}
-	int rightLim=n-1;
-	int rightsum=v[n-1];
-	for(int i=n-2;i>=leftLim;i--){
-		rightsum+=v[i];
-		if(ans-rightsum>ans){
-			ans-=rightsum;
-			rightLim=i;
-			rightsum=0;
+	/*
+	 * dp[i][j] represents the sum of all the numbers between i annd j 
+	 */
+	dp[0][0]=0;for(int i=1;i<sz;i++){
+		dp[0][i]+=v[i]+dp[0][i-1];
+	}
+
+	for(int i=1;i<sz;i++){
+		for(int j=i;i<=j && j<sz;j++){
+			dp[i][j]=dp[i-1][j]-dp[i-1][i-1];
 		}
 	}
+
+	for(int i=0;i<sz;i++){
+		cout<<endl;
+		for(int j=0;j<sz;j++){
+			if(j<i){
+				cout<<"\t";
+			} else {
+				//cout<<i<<","<<j<<"\t";	
+				cout<<dp[i][j]<<"\t";
+			}
+		}
+	}
+	cout<<endl;
 	return ans;
     }    
-       
 };
 
 void solve(){
 	int n;cin>>n; vector<int> v(n); for(int i=0;i<n;cin>>v[i++]);
 	Solution s;
 	int ans=s.maxSubArray(v);
-
 	cout<<ans;
 }
 
