ddb2c941a04224d0044d928ff83def4458f07b89
dhruvpatelgeek <dhruvpatel1999@outlook.in>
2021-08-26 10:41:29 -0700

    updated readme

---
+++ blind75/strings/ValidPalindrome/validPalindrome.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+	char s[1000];
+	cin.getline(s,1000);
+	cout<<s;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t; cin>>t;
+	while(t--){
+		solve();
+		cout<<endl;
+	}
+}
