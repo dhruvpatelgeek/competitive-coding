13638aed60d1889d6828a0f200412a537264a5b6
dhruvpatelgeek <dhruvpatel1999@outlook.in>
2021-08-26 19:50:18 -0700

    [blind 75] maximum product subarray [ARRAYS DONE]

--- blind75/arrays/MaximumProductSubarray/MaximumProductSubarray.cpp
+++ blind75/arrays/MaximumProductSubarray/MaximumProductSubarray.cpp
@@ -1,31 +1,41 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-
 class Solution {
 public:
     int maxProduct(vector<int>& v) {
         const int n=(int)v.size();
-	int **dp=new int*[2];
-	for(int i=0;i<2;i++){
-		dp[i]=new int[n];
-	}
+	int *min_dp=new int[n];
+	int *max_dp=new int[n]; 
+
+	 memset(min_dp,0,sizeof(min_dp[0])*n);
+         memset(max_dp,0,sizeof(max_dp[0])*n);
+	
+	 /* sorter of 
+	 * 		0 is min ele
+	 * 		2 is max ele
+	 */ 
+	int *sorter=new int[3];
 	int ans=v[0];
-	dp[0][0]=v[0];
-	dp[0][1]=v[0];
-	int *adj_dp=new int[3];
+	
+	min_dp[0]=v[0];
+	min_dp[0]=v[0];
 	for(int i=1;i<n;i++){
-		adj_dp[0]=v[i];
-		adj_dp[1]=dp[i-1][0]*v[i];
-		adj_dp[2]=dp[i-1][1]*v[i];
-		sort(adj_dp,adj_dp+3);	
-		dp[i][0]=adj_dp[0];
-		dp[i][1]=adj_dp[2];
-		ans=max(ans,dp[i][1]);
+		sorter[0]=v[i];
+		sorter[1]=v[i]*min_dp[i-1];
+		sorter[2]=v[i]*max_dp[i-1];
+		
+		sort(sorter,sorter+3);
+
+		min_dp[i]=sorter[0];
+		max_dp[i]=sorter[2];
+		ans=max(ans,max_dp[i]);
 	}
 
+	delete[] min_dp;
+	delete[] max_dp;
+	delete[] sorter;
 	return ans;
-
     }
 };
 
@@ -40,5 +50,8 @@ void solve(){
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	solve();cout<<endl;
+	int t; cin>>t;
+	while(t--){
+		solve(); cout<<endl;
+	}
 }
