66e6084b8c5c9efbefbacd1fc1c9927e2bc2fec1
Dhruv Patel <dhruvpatel1999@outlook.in>
2021-09-04 09:27:36 -0700

    Update minimumKnightMoves.cpp

--- leetcode_questions/MinimumKnightMoves/minimumKnightMoves.cpp
+++ leetcode_questions/MinimumKnightMoves/minimumKnightMoves.cpp
@@ -3,35 +3,34 @@
  *
  * link: https://leetcode.com/problems/minimum-knight-moves/submissions/
  *
- * correct but time limit excedded 
  */ 
-
-#include<bits/stdc++.h>
-
-using namespace std;
 
 class Solution {
 #define x first
 #define y second
 #define mp make_pair
 #define pb push_back
-
+private:
+    int max_size, offset;
 public:
     int minKnightMoves(int _x, int _y) {
+        _x = abs(_x);
+        _y = abs(_y);
+            
 	vector< pair<int,int> > path={
 		{2,1},{2,-1},{-2,1},{-2,-1},
 		{-1,2},{1,2},{1,-2},{-1,-2}
 	};
 	vector< vector <int> > todo;
 	todo.pb( vector<int> {0,0,0} );
-	set< pair<int,int> > done;
+	
+        offset = max(abs(_x), abs(_y)) + 8;
+        max_size = 2 * offset + 1;
+        vector<vector<bool>> done(max_size, vector<bool>(max_size));
+        done[offset][offset] = true;
+       
 	int nx,ny,nc;
 	for(int i=0;i<(int)todo.size();i++){
-		if (done.count(mp(todo[i][0],todo[i][1]))!=0) {
-			continue;		
-		}
-		done.insert(mp(todo[i][0],todo[i][1]));
-		
 		if(todo[i][0]==_x&&todo[i][1]==_y){
 			return todo[i][2];
 		}	
@@ -40,27 +39,14 @@ public:
 			nx=a.x+todo[i][0];	
 			ny=a.y+todo[i][1];	
 			nc=todo[i][2]+1;
-			//cout<<"\n the next paths are "<<nx<<" "<<ny<<"  with cost "<<nc;
-			if(done.count(mp(nx,ny))==0){
+			
+            if(nx >= -2 && ny >= -2 && max(abs(nx),abs(ny)) <= offset && !done[nx+offset][ny+offset]) {
+                done[nx+offset][ny+offset] = true;
 				todo.pb( vector <int> {nx,ny,nc} );
 			}
 		}
 	}
+       
 	return -1; 
     }
 };
-
-void solve(){
-	int x,y; cin>>x>>y;
-	Solution s;
-	cout<< s.minKnightMoves(x,y);
-}
-
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int t; cin>>t;
-	while(t--){
-		solve(); cout<<endl;
-	}
-}
